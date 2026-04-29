459e6dfeeb3bc016a82566d0c560d19f94d5e98b
binggwa <qudrhks317@gmail.com>
2026-04-29 17:30:23 +0900

    feat: ExpirationDateSection 컴포넌트 구현
    Co-authored-by: yundol777 <mar0722@naver.com>

---
+++ my-app/src/components/ExpirationDateSection/ExpirationDateSection.tsx
@@ -0,0 +1,40 @@
+import isInputValidate from '../../utils/Validation';
+import CommonSection from '../CommonSection/CommonSection';
+import NumberInput from '../NumberInput/NumberInput';
+
+interface Props {
+  value: {
+    month: string,
+    year: string
+  };
+  setValue: (value: { month: string, year: string }) => void;
+}
+
+export default function ExpirationDateSection({ value, setValue }: Props) {
+  function handleOnChange(inputValue: string, type: "month" | "year") {
+    if(!isInputValidate(inputValue, 2)) return;
+
+    const newValue = { ...value, [type]: inputValue };
+    setValue(newValue);
+  }
+
+  return (
+    <CommonSection
+      title="카드 유효기간을 입력해 주세요"
+      description="월/년도(MMYY)를 순서대로 입력해 주세요."
+      label="유효기간"
+      errorMessage="숫자만 입력 가능합니다."
+    >
+        <NumberInput
+          value={value.month}
+          onChange={(v) => handleOnChange(v, "month")}
+          placeholder="MM"
+        />
+         <NumberInput
+          value={value.year}
+          onChange={(v) => handleOnChange(v, "year")}
+          placeholder="YY"
+        />
+    </CommonSection>
+  );
+}
