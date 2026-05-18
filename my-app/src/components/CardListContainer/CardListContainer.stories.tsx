581f0cb0e2090245d68f6034fb4c3fc8331d4691
yundoll <mar0722@naver.com>
2026-05-18 12:50:56 +0900

    feat: 카드 목록 UI 컨테이너 구현
    - 카드 목록에서 공통으로 쓰이는 UI 컨테이너 구현
    - 스타일 및 스토리 구현

---
+++ my-app/src/components/CardListContainer/CardListContainer.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { cards } from '../../mocks/mockDB';
+import CardList from './CardList/CardList';
+import CardListEmpty from './CardListEmpty/CardListEmpty';
+import CardListError from './CardListError/CardListError';
+import CardListLoading from './CardListLoading/CardListLoading';
+import CardListContainer from './CardListContainer';
+
+const meta: Meta<typeof CardListContainer> = {
+  title: 'Components/CardListContainer',
+  component: CardListContainer,
+};
+
+export default meta;
+type Story = StoryObj<typeof CardListContainer>;
+
+export const List: Story = {
+  args: {
+    itemCount: cards.length,
+    children: <CardList cardList={cards} />,
+  },
+};
+
+export const Empty: Story = {
+  args: {
+    itemCount: 0,
+    children: <CardListEmpty />,
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    itemCount: null,
+    children: <CardListLoading />,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    itemCount: null,
+    children: <CardListError />,
+  },
+};
